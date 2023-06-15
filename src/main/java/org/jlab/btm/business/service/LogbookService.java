89bdbf80ea2e0cfecc8bd122056774b066677aa4
slominskir <ryan.slominski@gmail.com>
2023-06-15 14:02:06 -0400

    exp logbook support [no ci]

--- src/main/java/org/jlab/btm/business/service/LogbookService.java
+++ src/main/java/org/jlab/btm/business/service/LogbookService.java
@@ -6,6 +6,7 @@ import org.jlab.jlog.LogEntry;
 import org.jlab.jlog.LogEntryAdminExtension;
 import org.jlab.jlog.exception.LogCertificateException;
 import org.jlab.jlog.exception.LogIOException;
+import org.jlab.smoothness.persistence.enumeration.Hall;
 
 import javax.annotation.Resource;
 import javax.annotation.security.PermitAll;
@@ -69,6 +70,63 @@ public class LogbookService {
         LogEntry entry = new LogEntry(subject, logbooks);
 
         entry.setBody(html, Body.ContentType.HTML);
+
+        entry.setTags(tags);
+
+        LogEntryAdminExtension extension = new LogEntryAdminExtension(entry);
+        extension.setAuthor(username);
+
+        return entry.submitNow();
+    }
+
+    @PermitAll
+    public long createHallLogEntry(Hall hall, String subject, String html) throws IOException,
+            LogIOException, LogCertificateException {
+
+        String username = checkAuthorized(); /*Must be logged in*/
+
+        String logbookServerUrl = System.getenv("LOGBOOK_SERVER_URL");
+        String logbooks = "ELOG";
+        String tags = "BeamAccounting";
+
+        // In the absence of a test server an alternative is to use production server,
+        // but route entries to TLOG
+        String logbookDebug = System.getenv("LOGBOOK_DEBUG");
+
+        if("true".equals(logbookDebug)) {
+            logbooks = "TLOG";
+            tags = null;
+            logger.log(Level.INFO, "Using logbook TLOG");
+        } else {
+            switch (hall) {
+                case A:
+                    logbooks = "HALOG";
+                    break;
+                case B:
+                    logbooks = "HBLOG";
+                    break;
+                case C:
+                    logbooks = "HCLOG";
+                    break;
+                case D:
+                    logbooks = "HDLOG";
+                    break;
+            }
+        }
+
+        logger.log(Level.INFO, "Sending elog to logbook: {}", logbooks);
+
+        Properties config = Library.getConfiguration();
+
+        config.setProperty("SUBMIT_URL", logbookServerUrl + "/incoming");
+
+        logger.log(Level.INFO, "Sending elog to logbook: {0}", logbooks);
+
+        LogEntry entry = new LogEntry(subject, logbooks);
+
+        Body.ContentType type = Body.ContentType.HTML;
+
+        entry.setBody(html, type);
 
         entry.setTags(tags);
 
