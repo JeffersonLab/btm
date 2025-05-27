e83a55ccc8c3a747c5ce07071b776c845931f4cc
Ryan Slominski <Ryan.Slominski@gmail.com>
2025-05-27 17:28:55 -0400

    PVCache Support

--- src/main/java/org/jlab/btm/business/service/epics/CcEpicsMultiplicityHourService.java
+++ src/main/java/org/jlab/btm/business/service/epics/CcEpicsMultiplicityHourService.java
@@ -1,8 +1,8 @@
 package org.jlab.btm.business.service.epics;
 
-import com.cosylab.epics.caj.CAJContext;
 import gov.aps.jca.CAException;
 import gov.aps.jca.TimeoutException;
+import gov.aps.jca.dbr.DBR;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -13,8 +13,7 @@ import javax.ejb.Stateless;
 import org.jlab.btm.business.util.HourUtil;
 import org.jlab.btm.persistence.entity.CcHallHour;
 import org.jlab.btm.persistence.entity.CcMultiplicityHour;
-import org.jlab.btm.persistence.epics.MultiplicityBeamAvailability;
-import org.jlab.btm.persistence.epics.MultiplicityBeamAvailabilityDao;
+import org.jlab.btm.persistence.epics.*;
 
 /**
  * Responsible for querying experimenter hall EPICS time accounting data.
@@ -26,7 +25,7 @@ public class CcEpicsMultiplicityHourService {
 
   private static final Logger logger =
       Logger.getLogger(CcEpicsMultiplicityHourService.class.getName());
-  @EJB ContextFactory factory;
+  @EJB PVCache cache;
 
   public List<CcMultiplicityHour> find(
       Date startDayAndHour, Date endDayAndHour, List<List<CcHallHour>> hallHoursList)
@@ -52,22 +51,38 @@ public class CcEpicsMultiplicityHourService {
 
     MultiplicityBeamAvailability accounting;
 
-    CAJContext context = factory.getContext();
-
-    try {
-      MultiplicityBeamAvailabilityDao dao = new MultiplicityBeamAvailabilityDao(context);
-
-      long start = System.currentTimeMillis();
-      accounting = dao.loadAccounting();
-      long end = System.currentTimeMillis();
-      logger.log(
-          Level.FINEST,
-          "EPICS multiplicity hall hours load time (milliseconds): {0}",
-          (end - start));
-    } finally {
-      factory.returnContext(context);
-    }
+    long start = System.currentTimeMillis();
+    accounting = getFromCache();
+    long end = System.currentTimeMillis();
+    logger.log(
+        Level.FINEST, "EPICS multiplicity hall hours load time (milliseconds): {0}", (end - start));
 
     return accounting.getOpMultiplicityHours();
+  }
+
+  private MultiplicityBeamAvailability getFromCache() {
+    MultiplicityBeamAvailability accounting = new MultiplicityBeamAvailability();
+
+    List<DBR> dbrs = new ArrayList<>();
+
+    dbrs.add(cache.get(Constant.TIME_CHANNEL_NAME));
+    dbrs.add(cache.get(Constant.MULTI_ONE_UP));
+    dbrs.add(cache.get(Constant.MULTI_TWO_UP));
+    dbrs.add(cache.get(Constant.MULTI_THREE_UP));
+    dbrs.add(cache.get(Constant.MULTI_FOUR_UP));
+    dbrs.add(cache.get(Constant.MULTI_ANY_UP));
+    dbrs.add(cache.get(Constant.MULTI_ALL_UP));
+    dbrs.add(cache.get(Constant.MULTI_DOWN));
+
+    accounting.setTime(SimpleGet.getDoubleValue(dbrs.remove(0)));
+    accounting.setOneUp(SimpleGet.getDoubleValue(dbrs.remove(0)));
+    accounting.setTwoUp(SimpleGet.getDoubleValue(dbrs.remove(0)));
+    accounting.setThreeUp(SimpleGet.getDoubleValue(dbrs.remove(0)));
+    accounting.setFourUp(SimpleGet.getDoubleValue(dbrs.remove(0)));
+    accounting.setAnyUp(SimpleGet.getDoubleValue(dbrs.remove(0)));
+    accounting.setAllUp(SimpleGet.getDoubleValue(dbrs.remove(0)));
+    accounting.setDownHard(SimpleGet.getDoubleValue(dbrs.remove(0)));
+
+    return accounting;
   }
 }
