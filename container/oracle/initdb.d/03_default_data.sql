264e6c16f12a0a388c2d03b80829a22d87ec7bf8
Ryan Slominski <Ryan.Slominski@gmail.com>
2025-06-03 09:55:22 -0400

    Bump smoothness weblib to v4.13.0

--- container/oracle/initdb.d/03_default_data.sql
+++ container/oracle/initdb.d/03_default_data.sql
@@ -1,11 +1,5 @@
 alter session set container = XEPDB1;
 
--- Settings
-insert into BTM_OWNER.SETTING (KEY, VALUE, TYPE, DESCRIPTION, TAG, WEIGHT) values ('ADMIN_ROLE_NAME', 'btm-admin', 'STRING', 'App-specific Admin Role Name', 'AUTH', 1);
-insert into BTM_OWNER.SETTING (KEY, VALUE, TYPE, DESCRIPTION, TAG, WEIGHT) values ('SMOOTHNESS_CDN', 'N', 'BOOLEAN', 'Smoothness weblib resources from CDN.  Defaults to No = serve files locally. CDN is for minified/combined files on shared Content Delivery Network (CDN) server - Nice for when multiple apps use same resources to have warm cache.', 'CDN', 1);
-insert into BTM_OWNER.SETTING (KEY, VALUE, TYPE, DESCRIPTION, TAG, WEIGHT) values ('SMOOTHNESS_SERVER', 'ace.jlab.org/cdn', 'STRING', 'Host name and port of content delivery network host for shared smoothness resources. Only used if SMOOTHNESS_LOCATION=CDN', 'CDN', 2);
-insert into BTM_OWNER.SETTING (KEY, VALUE, TYPE, DESCRIPTION, TAG, WEIGHT) values ('SMOOTHNESS_VERSION', '4.11.0', 'STRING', 'Version of smoothness lib on CDN.  Only used if SMOOTHNESS_LOCATION=CDN', 'CDN', 3);
-
 -- EXP_PROGRAM
 INSERT INTO BTM_OWNER.EXP_PROGRAM (EXP_PROGRAM_ID, HALL, NAME, ACTIVE, EXPERIMENT, ALIAS, URL) VALUES (1, 'A', 'Facility Development', '0', '0', null, null);
 INSERT INTO BTM_OWNER.EXP_PROGRAM (EXP_PROGRAM_ID, HALL, NAME, ACTIVE, EXPERIMENT, ALIAS, URL) VALUES (2, 'A', 'Physics', '0', '0', null, null);
