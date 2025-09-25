dcc158d1103f28f7f5c999de173fccc2aab8699c
Ryan Slominski <Ryan.Slominski@gmail.com>
2025-09-25 17:04:14 -0400

    Format

--- src/main/java/org/jlab/btm/persistence/entity/audit/ExpShiftAud.java
+++ src/main/java/org/jlab/btm/persistence/entity/audit/ExpShiftAud.java
@@ -19,7 +19,7 @@ public class ExpShiftAud implements Serializable {
   private static final long serialVersionUID = 1L;
   @EmbeddedId protected ExpShiftAudPK expShiftAudPK;
 
-  //@Enumerated(EnumType.ORDINAL)
+  // @Enumerated(EnumType.ORDINAL)
   @NotNull
   @Column(name = "REVTYPE")
   private RevisionType type;
